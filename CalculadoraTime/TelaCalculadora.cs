53f9c176d46d28f17f037b6055c5280e561cecc5
diego <diego@DESKTOP-DIHC3EH>
2023-06-07 15:18:20 -0300

    Tela

---
+++ CalculadoraTime/TelaCalculadora.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraTime
+{
+    internal class TelaCalculadora
+    {
+        public static void ExibirMenuCalculadora()
+        {
+            Console.WriteLine("Calculadora");
+            Console.WriteLine("Selecione uma operacao");
+            Console.WriteLine("1 Para somar!");
+            Console.WriteLine("2 Para Subtracao");
+            Console.WriteLine("3 Para Multiplicacao");
+            Console.WriteLine("4 Para divisao");
+            Console.WriteLine("5 para sair");
+        }
+    }
+}
