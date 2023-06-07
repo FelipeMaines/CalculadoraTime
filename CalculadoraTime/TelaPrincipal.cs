3f905ec1122e8e925102b83b75fc14bb63cbb694
jggomes14 <jggomes141097@gmail.com>
2023-06-07 15:02:05 -0300

    Configuração da Tela Principal.

---
+++ CalculadoraTime/TelaPrincipal.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraTime
+{
+    internal class TelaPrincipal
+    {
+        public string ApresentarMenu()
+        { 
+
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+
+            Console.WriteLine("╔══════════════════════════════════╗");
+            Console.WriteLine("║     Calculadora Tabajara         ║");
+            Console.WriteLine("╠══════════════════════════════════╣");
+            Console.WriteLine("║ 1.  Efetuar uma Soma             ║");
+            Console.WriteLine("║ 2.  Efetuar uma Subtração        ║");
+            Console.WriteLine("║ 3.  Efetuar uma Multiplicação    ║");
+            Console.WriteLine("║ 4.  Efetuar uma Divisão          ║");
+            Console.WriteLine("║ 0.  Fechar a Calculadora         ║");
+            Console.WriteLine("╚══════════════════════════════════╝");
+            Console.Write("Digite a opção desejada: ");
+
+            string opcao = Console.ReadLine();
+
+            return opcao;
+
+
+
+
+        }
+    }
+}
